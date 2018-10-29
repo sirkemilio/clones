c633e483de1a4c5ed7bd27478942c961d8beecd2
Roberto <daw202027@estudiantes.salesianasnsp.es>
2018-10-29 10:26:16 +0100

    Update Clon.java

--- clon/src/clon/Clon.java
+++ clon/src/clon/Clon.java
@@ -16,6 +16,7 @@ public class Clon {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Hola mundo ;)");
     }
     
 }
